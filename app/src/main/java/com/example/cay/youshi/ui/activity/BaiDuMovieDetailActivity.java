765c978f11542b1bfcd2a67502d8d9e6b8abc8b9
Cay-chen <276495166@qq.com>
2017-03-14 23:16:49 +0800

    更改了电影详情 显示百度链接

--- app/src/main/java/com/example/cay/youshi/ui/activity/BaiDuMovieDetailActivity.java
+++ app/src/main/java/com/example/cay/youshi/ui/activity/BaiDuMovieDetailActivity.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.GridLayoutManager;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -48,6 +54,7 @@ public class BaiDuMovieDetailActivity extends BaseHeaderActivity<HeaderSlideShap
     private String name;
     private String baidu_url;
     private static final String TAG = "Cay";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,7 +89,7 @@ public class BaiDuMovieDetailActivity extends BaseHeaderActivity<HeaderSlideShap
                     }
 
                     @Override
-                    public void onError(Throwable e ){
+                    public void onError(Throwable e) {
                         showError();
                     }
 
@@ -107,29 +114,66 @@ public class BaiDuMovieDetailActivity extends BaseHeaderActivity<HeaderSlideShap
         bindingHeaderView.tvOneGenres.setText(this.getResources().getString(R.string.string_type) + subjectsBean.getGenres());
         bindingContentView.tvOneContiont.setText(subjectsBean.getLog());
         bindingContentView.tvOneTitle.setText(subjectsBean.getOther_name());
-        if (subjectsBean.getSubtype().equals("1")) {
-            if (subjectsBean.getNow_num().equals(subjectsBean.getTotal_num())) {
-                bindingContentView.btnYoushiMovieDetail.setText(subjectsBean.getNow_num() + "集全");
-            } else {
-                bindingContentView.btnYoushiMovieDetail.setText("已更新至" + subjectsBean.getNow_num() + "集");
-            }
+        String baidu_url = subjectsBean.getBaidu_url().trim();
+        bindingContentView.tvBaiduUrl.setMovementMethod(LinkMovementMethod.getInstance());
+        if (baidu_url.contains(" ")) {
+            final String[] urls = baidu_url.trim().split(" ");
+            SpannableString spannableString = new SpannableString(baidu_url);
+            spannableString.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(urls[0].substring(3).trim()));
+                    startActivity(intent);
+                }
+            }, 3, baidu_url.indexOf(" "), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            bindingContentView.tvBaiduUrl.setText(spannableString);
+
+
+            bindingContentView.btnCopyPassword.setVisibility(View.VISIBLE);
+            bindingContentView.btnCopyPassword.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    // 将文本内容放到系统剪贴板里。
+                    cm.setText(urls[1].substring(3));
+                    Toast.makeText(BaiDuMovieDetailActivity.this, "密码复制成功", Toast.LENGTH_LONG).show();
+                }
+            });
+            bindingContentView.btnCopyUrlPassword.setText("复制链接和密码");
+            bindingContentView.btnCopyUrlPassword.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    // 将文本内容放到系统剪贴板里。
+                    cm.setText(subjectsBean.getBaidu_url().trim());
+                    Toast.makeText(BaiDuMovieDetailActivity.this, "复制成功", Toast.LENGTH_LONG).show();
+                }
+            });
         } else {
-            bindingContentView.btnYoushiMovieDetail.setText("高清中字");
-        }
-        bindingContentView.btnYoushiMovieDetail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (subjectsBean.getBaidu_url().trim() != null) {
+            SpannableString spannableString = new SpannableString(baidu_url);
+            spannableString.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(View view) {
                     Intent intent = new Intent(Intent.ACTION_VIEW);
                     intent.setData(Uri.parse(subjectsBean.getBaidu_url().trim()));
                     startActivity(intent);
                 }
+            }, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            bindingContentView.tvBaiduUrl.setText(spannableString);
+            bindingContentView.btnCopyUrlPassword.setText("复制链接");
+            bindingContentView.btnCopyUrlPassword.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    // 将文本内容放到系统剪贴板里。
+                    cm.setText(subjectsBean.getBaidu_url().trim());
+                    Toast.makeText(BaiDuMovieDetailActivity.this, "复制成功", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
 
-            }
-        });
     }
-
-
 
 
     @Override
@@ -163,7 +207,7 @@ public class BaiDuMovieDetailActivity extends BaseHeaderActivity<HeaderSlideShap
     protected ImageView setHeaderImageView() {
         return bindingHeaderView.imgItemBg;
     }
-    
+
     public static void start(Activity context, String id, String img_url, ImageView imageView) {
         Intent intent = new Intent(context, BaiDuMovieDetailActivity.class);
         intent.putExtra("id", id);
@@ -180,13 +224,12 @@ public class BaiDuMovieDetailActivity extends BaseHeaderActivity<HeaderSlideShap
     }
 
 
-
     @Override
     protected void onRefresh() {
         bindingContentView.activityOneMovieDetail.postDelayed(new Runnable() {
             @Override
             public void run() {
-                    onLoadData(movieId);
+                onLoadData(movieId);
 
             }
         }, 1000);
